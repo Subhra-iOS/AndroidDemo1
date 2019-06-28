f6ed9ef092bb6dd6d495c0bde39d24da5001f21a
Subhra Roy <subhra.roy84@yahoo.in>
2019-06-28 19:10:10 +0530

    add btn action

--- app/src/main/java/com/example/androiddemo1/MainActivity.kt
+++ app/src/main/java/com/example/androiddemo1/MainActivity.kt
@@ -2,13 +2,23 @@ package com.example.androiddemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : Button = findViewById(R.id.tapOnMe)
+        button.setOnClickListener{
+            // Do something in response to button click
+                print("Test")
+        }
+
+
     }
+
 
 
 
