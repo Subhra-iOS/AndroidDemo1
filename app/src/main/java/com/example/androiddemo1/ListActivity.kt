35ac7522c74d63274bd8a76ba3baf84ed29baf05
Subhra Roy <subhra.roy84@yahoo.in>
2019-07-18 17:22:13 +0530

    Add demo navigation

--- app/src/main/java/com/example/androiddemo1/ListActivity.kt
+++ app/src/main/java/com/example/androiddemo1/ListActivity.kt
@@ -1,4 +1,22 @@
 package com.example.androiddemo1
 
-class ListActivity {
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val textView = findViewById<TextView>(R.id.textLabel).apply {
+            this.text = message
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/androiddemo1/MainActivity.kt
+++ app/src/main/java/com/example/androiddemo1/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.androiddemo1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+
+const val EXTRA_MESSAGE = "com.example.androiddemo1.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,15 +14,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
         val button  = findViewById<Button>(R.id.launchButton)
         button.setOnClickListener{
             // Do something in response to button click
-               // Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
-              print("Test")
-        }
+            Toast.makeText(this, "Test", Toast.LENGTH_LONG).show()
 
+            val listIntent = Intent(this, ListActivity::class.java).apply {
+                    putExtra(EXTRA_MESSAGE, "Open List")
+            }
+
+            startActivity(listIntent)
+        }
 
     }
 
